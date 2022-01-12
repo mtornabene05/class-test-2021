b6ae2bfa99d9d77456a77c7cdae892d4cda0492b
Maria <mtornabene05@gmail.com>
2022-01-12 18:39:22 -0500

    beginning of finalizations

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,21 @@
 })(jQuery); // end of jQuery name space
 
 
+ // Load the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+ tag.src = "https://www.youtube.com/player_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+ // Replace the 'ytplayer' element with an <iframe> and
+ // YouTube player after the API code downloads.
+ var player;
+ function onYouTubePlayerAPIReady() {
+     player = new YT.Player('ytplayer', {
+     height: '360',
+     width: '640',
+     videoId: '${data.items[0].id.videoId}'
+     });
 
 
 
@@ -69,4 +83,4 @@
 
 // var loadYoutubeData = function() {
 
-// }
+ }
